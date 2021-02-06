bfe763155751f3e5684b3a0f5df0693cf57245f8
Rodrigo Gonzalez <49202252+Xer0-0reX@users.noreply.github.com>
2021-02-06 13:33:17 -0600

    Tarea 2

---
+++ listas.pl
@@ -0,0 +1,16 @@
+sumlista([],0).
+sumlista([X|L],N) :-
+	sumlista(L,C),
+	N is C + X.
+
+revertir([],[]).
+revertir([X|T],N):- revertir([X|T],[],N).
+revertir([],Zs,Zs).
+revertir([X|Xs],Ys,Zs):- revertir(Xs,[X|Ys],Zs).
+
+rango(I,I,[I]).
+rango(I,K,[I|L]):-
+	I<K,
+	I1 is I + 1,
+	rango(I1,K,L).
+
